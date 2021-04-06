3b1e79051d4fa97d7e22f19211d4fc20ac553cd2
PatrickAlphaC <54278053+PatrickAlphaC@users.noreply.github.com>
2021-04-06 19:37:04 -0400

    updated for better logging

--- scripts/chainlink_api_scripts/read_data.py
+++ scripts/chainlink_api_scripts/read_data.py
@@ -6,5 +6,5 @@ def main():
     api_contract = APIConsumer[len(APIConsumer) - 1]
     print("Reading data from {}".format(api_contract.address))
     if api_contract.volume() == 0:
-        print("You may have to wait a minute and then call this again!")
+        print("You may have to wait a minute and then call this again, unless on a local chain!")
     print(api_contract.volume())
--- scripts/vrf_scripts/read_random_number.py
+++ scripts/vrf_scripts/read_random_number.py
@@ -7,5 +7,5 @@ STATIC_SEED = 123
 def main():
     vrf_contract = VRFConsumer[len(VRFConsumer) - 1]
     if vrf_contract.randomResult() == 0:
-        print("The result is 0, you may have to wait a minute!")
+        print("The result is 0, you may have to wait a minute unless on a local chain!")
     print(vrf_contract.randomResult())
