2432e20ad14e075bd899dcac359340188c45c99a
PatrickAlphaC <54278053+PatrickAlphaC@users.noreply.github.com>
2021-07-02 12:25:12 -0400

    fix keeper test

--- tests/test_keeper.py
+++ tests/test_keeper.py
@@ -18,10 +18,10 @@ def test_can_call_check_upkeep():
     assert isinstance(performData, bytes)
     assert upkeepNeeded is False
     # Since the interval is 2, after 2 seconds the upkeep should return true
-    # We are extra safe and just make it 4
-    time.sleep(4)
+    # We are extra safe and just make it 6
+    time.sleep(6)
     upkeepNeeded, performData = counter.checkUpkeep.call(
         "",
         {"from": account},
     )
-    assert upkeepNeeded is False
+    assert upkeepNeeded is True
